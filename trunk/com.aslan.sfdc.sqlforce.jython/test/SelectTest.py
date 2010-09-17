3a8da0714f50a45e28b2ba66a702cd63c1e969b7
gsmithfarmer <gsmithfarmer@71af4556-f3f6-11de-99d0-77da0b7ae2c6>
2010-09-17 13:06:22 +0000

    Fix problem in jython select parser for select a.b.c, a.d.e case.

--- trunk/com.aslan.sfdc.sqlforce.jython/test/SelectTest.py
+++ trunk/com.aslan.sfdc.sqlforce.jython/test/SelectTest.py
@@ -19,6 +19,13 @@ class SelectTest( unittest.TestCase):
         session = TestEnvironment.getSession()
         n = session.select("SELECT COUNT() FROM Contact")[0][0]
         self.assertTrue(n)
+
+    def testSelectDeepPath(self):
+        session = TestEnvironment.getSession()
+        session.selectRecords("SELECT account.owner.name, account.lastModifiedBy.alias FROM contact LIMIT 1")[0]
+        '''
+        The previous line would crash because of a deep path on 15-Sep-10 (release 1.0.7)
+        '''
         
 if __name__=='__main__':
     unittest.main()
