244d42256b80e15f735b45c3ef7c070a300668d4
gsmithfarmer <gsmithfarmer@71af4556-f3f6-11de-99d0-77da0b7ae2c6>
2010-09-18 01:55:19 +0000

    Start framework for mass extraction of attachments.

---
+++ trunk/com.aslan.sfdc.extract.test/src/com/aslan/sfdc/extract/test/ExtractAttachmentsTest.java
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Gregory Smith (gsmithfarmer@gmail.com)
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
+
+/**
+ * Extract attachments (lots of them).
+ */
+package com.aslan.sfdc.extract.test;
+
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import com.aslan.sfdc.extract.DefaultExtractionMonitor;
+import com.aslan.sfdc.extract.ExtractionManager;
+import com.aslan.sfdc.extract.ExtractionRuleset;
+import com.aslan.sfdc.extract.IDatabaseBuilder;
+import com.aslan.sfdc.extract.ExtractionRuleset.TableRule;
+import com.aslan.sfdc.partner.LoginManager;
+import com.aslan.sfdc.partner.test.SfdcTestEnvironment;
+import com.sforce.soap.partner.DescribeSObjectResult;
+import com.sforce.soap.partner.Field;
+
+public class ExtractAttachmentsTest extends TestCase {
+
+	private class DatabaseScanner implements IDatabaseBuilder {
+
+		@Override
+		public void createTable(DescribeSObjectResult sfdcTable)
+				throws Exception {
+			System.err.println("Create Table: " + sfdcTable.getName());
+			
+		}
+
+		@Override
+		public void insertData(DescribeSObjectResult sfdcTable, Field[] fields,
+				List<String[]> dataRows) throws Exception {
+			for( String[] row : dataRows ) {
+				System.err.println(row[0]);
+			}
+			
+		}
+		
+	}
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testGetAttachments() throws Exception {
+		//LoginManager.Session session = SfdcTestEnvironment.getSession("Readonly");
+		LoginManager.Session session = SfdcTestEnvironment.getTestSession();
+		DatabaseScanner scanner = new DatabaseScanner();
+		ExtractionManager mgr = new ExtractionManager(session, scanner);
+		ExtractionRuleset rules = new ExtractionRuleset();
+		
+		rules.includeTable(new TableRule("Attachment", true));
+		mgr.extractData( rules, new DefaultExtractionMonitor());
+			
+
+	}
+
+
+
+}
